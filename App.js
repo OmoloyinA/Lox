16f04be8c6a1863d37a11b89a7bd967a55cf8e65
Wrokbrook repo <chukschiboy@gmail.com>
2022-01-12 23:43:45 +0100

    creating the navigation options component for the social service app

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 
 // Set up Redux
-import store from './store';
+import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>LET'S BUILD CHIDINMA AMUTA</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
