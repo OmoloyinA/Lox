980299af11e180a6ccb050f98c88d548a5d10836
Wrokbrook repo <chukschiboy@gmail.com>
2022-02-16 15:12:21 +0100

    creating the autocomplete location for the Errands App

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import NavOptions from '../components/NavOptions';
 
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
 const HomeScreen = () => {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
@@ -12,6 +15,20 @@ const HomeScreen = () => {
                     source={{
                         uri: "https://links.papareact.com/gzs",
                     }}
+                />
+
+                <GooglePlacesAutocomplete 
+                    placeholder='Where From?'
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
                 />
 
                 <NavOptions />
